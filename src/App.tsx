f66e3336ae226a193a211e51296aea1f0538ab3a
Javohir9026 <javohiramanbayev9026@gmail.com>
2026-02-11 22:35:18 +0500

    created

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LandingPage from "./pages/Landing/LandingPage";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index path="/" element={<LandingPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/pages/Landing/LandingPage.tsx
@@ -0,0 +1,16 @@
+import Header from './sections/Header'
+import Footer from './sections/Footer'
+
+const LandingPage = () => {
+  return (
+    <div>
+        <Header/>
+        <div className='container'>
+            MAIN
+        </div>
+        <Footer/>
+    </div>
+  )
+}
+
+export default LandingPage
