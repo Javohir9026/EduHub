33f3fb02c1a20abe8191f9120ef7b63e339698c5
behruz2737 <behruzmatyusupov5@gmail.com>
2026-02-12 16:18:34 +0500

    hero ozgaritirildi

--- src/pages/Landing/sections/Hero.tsx
+++ src/pages/Landing/sections/Hero.tsx
@@ -1,10 +1,15 @@
+import {Button} from "@/components/ui/button"
+
 const Hero = () => {
   return (
-    <div className="container">
-        <div className="">
-            <div className="w-[500px]">
-                <h1 className="!font-bold text-5xl">Manage Your Education Center with Ease</h1>
-                <p >Complete management system for educational centers. Handle teachers, students, salary calculations, payments, groups, and attendance all in one place.</p>
+    <div className="">
+        <div className='container'>
+            <div className="w-[400px] mt-25">
+                <h1 className="!font-bold text-5xl">Ta'lim markazini oson boshqarish</h1> <br />
+                <p >Ta'lim markazlari uchun to'liq boshqaruv tizimi. O'qituvchilar, talabalar, ish haqi hisob-kitoblari, to'lovlar, guruhlar va davomatni bir joyda boshqaring.</p>
+            <Button variant="outline" className="bg-blue-500 text-white !font-semibold hover:bg-blue-500/85 hover:text-white cursor-pointer">Ro'yxatdan o'tish</Button>
+            <Button variant="outline" className="cursor-pointer !font-semibold hover:text-white hover:bg-purple-500">Kirish</Button>
+            
             </div>
         </div>
     </div>
