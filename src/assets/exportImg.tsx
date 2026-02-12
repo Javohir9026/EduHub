181641cce0c7f19db13ab2672adae1974d7e5425
Javohir9026 <javohiramanbayev9026@gmail.com>
2026-02-12 15:47:30 +0500

    header

---
+++ src/assets/exportImg.tsx
@@ -0,0 +1,4 @@
+export { default as EduHubDarkText } from "./icons/EduHubDarkText.png";
+export { default as EduHubLightText } from "./icons/EduHubLightText.png";
+export { default as EduHubDarkLogo } from "./icons/EduHubDarkLogo.png";
+export { default as EduHubLightLogo } from "./icons/EduHubLightLogo.png";
--- src/pages/Landing/sections/Header.tsx
+++ src/pages/Landing/sections/Header.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import { EduHubLightText } from "@/assets/exportImg";
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className="py-3 border-b border-black/30">
+      <div className="container">
+        <div className="w-30 overflow-hidden flex items-center justify-center">
+          <img src={EduHubLightText} alt="LogoLightText" />
+        </div>
+        <div>
+          
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
