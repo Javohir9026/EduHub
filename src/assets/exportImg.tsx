d8b2aef87d89c821fbfaae282e80f5600baf0b9d
behruz2737 <behruzmatyusupov5@gmail.com>
2026-02-15 00:41:32 +0500

    form ozgardi

--- src/assets/exportImg.tsx
+++ src/assets/exportImg.tsx
@@ -3,3 +3,4 @@ export { default as EduHubHeroImg } from "./Images/heroimg.avif";
 export { default as EduHubBlue } from "./Images/Blue.png";
 export { default as EduLogoLight } from "./icons/EduLogoLight.png";
 export { default as EduLogoDark } from "./icons/EduLogoDark.png";
+export {default as EduHubSignInImg} from "./icons/signin.jpg"
