cb9f7cdb658928a7e3c917e5e57838df1b42a321
Javohir9026 <javohiramanbayev9026@gmail.com>
2026-03-06 21:14:31 +0500

    groupeditmodal 109 row undefined error fixed

--- src/components/common/Group/GroupEditModal.tsx
+++ src/components/common/Group/GroupEditModal.tsx
@@ -106,7 +106,7 @@ export function GroupEditModal({
     if (!validate()) return;
 
     try {
-      const numericPrice = Number(monthlyPrice.replace(/\D/g, "")).toFixed(1git);
+      const numericPrice = Number(monthlyPrice.replace(/\D/g, "")).toFixed(1);
       setLoading(true);
       console.log(
         name,
