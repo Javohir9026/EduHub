54168a366b648cf25854931bb1d35c9530bbeef5
behruz2737 <behruzmatyusupov5@gmail.com>
2026-03-06 21:15:00 +0500

    dashbord yangilandi 2

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -5,11 +5,32 @@ import { useEffect, useState } from "react";
 
 const Dashboard = () => {
 
+  const [studentsCount, setStudentsCount] = useState<number>(0);
+
   useEffect(() => {
-}, []);
+
+    const fetchStudentsCount = async () => {
+      try {
+        const token = localStorage.getItem("access_token");
+        const api = import.meta.env.VITE_API_URL;
+        const learningCenterId = localStorage.getItem('id')
+        const res = await apiClient.get(`${api}/learning-centers/${learningCenterId}/students`, {
+          headers: { Authorization: `Bearer ${token}` }
+        });
+
+        setStudentsCount(res.data.data.length);
+        
+      } catch (error) {
+        console.error("Students count olishda xatolik", error);
+      }
+    };
+    
+    fetchStudentsCount();
+
+  }, []);
 
   const stats = [
-    { id:1, icon:<Users />, title:"Jami o`quvchilar", count: 12, persend:"+ O'tgan oyga nisbatan 12%" },
+    { id:1, icon:<Users />, title:"Jami o`quvchilar", count: studentsCount, persend:"+ O'tgan oyga nisbatan 12%" },
     { id:2, icon:<Coins />, title:"Oylik daromad", count:"$25,480", persend:"+ 8,5% ga o'sish" },
     { id:3, icon:<Users />, title:"O'rtacha ishtirok", count:"92%", persend:"+ O'tgan haftada 2% ga oshgan" },
     { id:4, icon:<ChevronUpSquareIcon />, title:"Faol ustozlar", count:"42", persend:"+ Bu chorakda 5 ta yangi kurslar" },
