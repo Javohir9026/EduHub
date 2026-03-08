eed19636d4ece94d6ff70e1f4f34ad30527e7529
Javohir9026 <javohiramanbayev9026@gmail.com>
2026-03-08 20:18:16 +0500

    breadcrumb addded calendar

--- src/pages/Calendar/CalendarPage.tsx
+++ src/pages/Calendar/CalendarPage.tsx
@@ -1,8 +1,21 @@
+import { BreadcrumbBasic } from "@/components/common/BreadCrumb";
 
 const CalendarPage = () => {
   return (
-    <div>CalendarPage</div>
-  )
-}
+    <div>
+      <div className="flex justify-end">
+        <BreadcrumbBasic
+          items={[
+            { title: "Bosh sahifa", href: "/" },
+            { title: "Kalendar", href: "/calendar" },
+          ]}
+        />
+      </div>
+      <div className="p-6 space-y-8">
+        Calendar Content...
+      </div>
+    </div>
+  );
+};
 
-export default CalendarPage
+export default CalendarPage;
